a3a147edfc2f65deeb3c0f43c9b2f2948bee2df6
arshavir777 <arshogharibyan@gmail.com>
2023-09-11 17:38:33 +0400

    init

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid">
+    <div class="row bg-dark text-white justify-content-center py-3">
+      <div class="col-auto">
+        <a href="#" class="text-decoration-none text-white">
+          <Logo />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Logo from "../Logo/Logo.vue";
+
+export default {
+  name: 'Header',
+  components: {Logo},
+}
+</script>
---
+++ src/components/Logo/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="d-flex align-items-center" href="#">
+    <img src="../../assets/hamburger.png" alt="logo">
+    <span class="ms-3 fw-bold">Y O L O</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+}
+</script>
